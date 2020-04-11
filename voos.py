40f8ff59e477718e792232d340daf5233acf079b
Alvarole <leandro0807@live.com>
2020-04-11 10:21:07 -0300

    adicionado numero maximo de iterações e continuando em caso de ser igual

--- voos.py
+++ voos.py
@@ -32,3 +32,49 @@ def imprimir_agenda(agenda):
         
 agenda = [1,4, 3,2, 7,3, 6,3, 2,4, 5,3]
 imprimir_agenda(agenda)
+
+def get_minutos(hora):
+    x = time.strptime(hora, '%H:%M')
+    minutos = x[3] * 60 + x[4]
+    return minutos
+
+def funcao_custo(solucao):
+    preco_total = 0
+    ultima_chegada = 0
+    primeira_partida = 1439
+    
+    id_voo = -1
+    for i in range(len(solucao) // 2):
+        origem = pessoas[i][1]
+        id_voo += 1
+        ida = voos[(origem, destino)][solucao[id_voo]]
+        id_voo += 1
+        volta = voos[(destino, origem)][solucao[id_voo]]
+        
+        preco_total += ida[2]
+        preco_total += volta[2]
+        
+        if ultima_chegada < get_minutos(ida[1]):
+            ultima_chegada = get_minutos(ida[1])
+            
+        if primeira_partida > get_minutos(volta[0]):
+            primeira_partida = get_minutos(volta[0])
+            
+    total_espera = 0
+    id_voo = -1
+    for i in range(len(solucao) // 2):
+        origem = pessoas[i][1]
+        id_voo += 1
+        ida = voos[(origem, destino)][solucao[id_voo]]
+        id_voo += 1
+        volta = voos[(destino, origem)][solucao[id_voo]]
+        
+        total_espera += ultima_chegada - get_minutos(ida[1])
+        total_espera += get_minutos(volta[0]) - primeira_partida
+        
+    if ultima_chegada > primeira_partida:
+        preco_total += 50
+        
+    return preco_total + total_espera
+        
+funcao_custo(agenda)  
