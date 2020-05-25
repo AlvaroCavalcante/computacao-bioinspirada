6e9668577308e8b589b1f8e4a4aae9dbd954d100
Alvarole <leandro0807@live.com>
2020-05-25 16:29:00 -0300

    atualizado a perceptron por epoca

--- redes neurais/perceptron2.py
+++ redes neurais/perceptron2.py
@@ -58,7 +58,6 @@ classe_nova = []
 for i in classe:
     classe_nova.append(classe_codificada[i])
     
-
 classe_nova = np.array(classe_nova).reshape(150,3)
 
 def substituir_classe_codificada(valor, classe_codificada):
@@ -66,25 +65,35 @@ def substituir_classe_codificada(valor, classe_codificada):
 
 classe = classe.apply(lambda row: substituir_classe_codificada(row, classe_codificada))
 
-def inicializar_pesos():
+def dividir_dataframe(previsores, classe, p_treinamento, p_teste, p_validacao):
+    x_treinamento = previsores.sample(frac = p_treinamento)
+    y_treinamento = classe[x_treinamento.index]
+    
+    x_teste_sem_previsores = previsores.drop(x_treinamento.index)
+    nova_p_teste = p_teste / (1 - p_treinamento)
+    
+    x_teste = x_teste_sem_previsores.sample(frac = nova_p_teste)
+    y_teste = classe[x_teste.index]
+    
+    x_validacao = x_teste_sem_previsores.drop(x_teste.index)
+    y_validacao = classe[x_validacao.index]
+    
+    return x_treinamento.reset_index(drop=True), y_treinamento, \
+    x_teste.reset_index(drop=True), y_teste, x_validacao.reset_index(drop=True), y_validacao
+
+
+def inicializar_pesos(dominio):
     pesos_final = []
     
     for i in range(len(previsores.columns)):
         pesos = [] 
         for j in range(len(dict_classes)):
-            pesos.append(random.random())
+            pesos.append(random.uniform(dominio[0], dominio[1]))
         pesos_final.append(pesos)
     return pesos_final
 
-pesos = inicializar_pesos()
-
 def somatoria(entradas, pesos):
     return np.dot(entradas, pesos)    
-
-def funcao_ativacao(soma):
-    soma[soma >= 0] = 1
-    soma[soma < 0] = 0
-    return soma
 
 def funcao_ativacao_sigmoid(soma):
     resultado = 1 / (1 + math.e ** -soma)
@@ -100,41 +109,68 @@ def funcao_ativacao_sigmoid(soma):
     
 def funcao_custo(valor_correto, valor_previsto):
     erro = list(abs(np.array(valor_correto) - np.array(valor_previsto)))
-    return sum(sum(erro)) # valor escalar
+    acerto = 0
+    
+    for i in erro:
+        if sum(i) == 0:
+            acerto += 1
+    
+    return sum(sum(erro)), acerto # valor escalar
 
-def atualizar_peso(entrada, peso, erro, tx_aprendizado = 0.001):
-    novo_peso = peso + (tx_aprendizado * entrada * erro)
+def atualizar_peso(entrada, peso, erro, tx_aprendizado = 0.01):
+    novo_peso = peso + sum((tx_aprendizado * entrada * erro))
     return novo_peso
 
-def treinar(epocas):
+def treinar(epocas, funcao_ativacao, funcao_custo, pesos, x_treinamento, y_treinamento,
+                                     x_teste, y_teste):
     execucoes = 0
+    precisoes_treinamento = []
     while execucoes < epocas:
-        precisao = 0
-        iteracao = 0
 
-        np.random.shuffle(previsores.values) # embaralhar os valores dos previsores, por que sem isso, podemos ter sempre uma ordem fixa de ajuste de pesos, prejudicando a rede
+        np.random.shuffle(x_treinamento.values) # embaralhar os valores dos previsores, por que sem isso, podemos ter sempre uma ordem fixa de ajuste de pesos, prejudicando a rede
 
-        entradas = previsores.values   
+        entradas = x_treinamento.values   
         soma = somatoria(entradas, pesos)
     
         ativacao = funcao_ativacao_sigmoid(soma)
     
-        erro = funcao_custo(classe_nova, ativacao) # baseado no meu resultado previsto, dado na última função de ativação.
+        erro, acertos = funcao_custo(y_treinamento, ativacao) # baseado no meu resultado previsto, dado na última função de ativação.
     
-        if erro > 0:
-            count = 0
-                
-            for i in range(entradas.shape[1]): # o for tem que atualizar cada peso da camada
-                novo_peso = atualizar_peso(entradas[:, i], pesos[i], erro)
-                pesos[count] = novo_peso
-                count += 1
-        else:
-            precisao += len(previsores) / 100
-            print('Precisão: ', precisao)
+        count = 0
+        precisoes_treinamento.append(acertos / len(x_treinamento))    
 
-        iteracao += 1
+        for i in range(entradas.shape[1]): # o for tem que atualizar cada peso da camada
+            novo_peso = atualizar_peso(entradas[:, i], pesos[i], erro)
+            pesos[count] = novo_peso
+            count += 1
         
         execucoes += 1
-    print('Precisão final: ', precisao)
+    
+    return precisoes_treinamento
 
-treinar(500)
+previsores['bias'] = 1
+
+
+def executar_perceptron(funcao_ativacao, funcao_custo, epocas, dominio_pesos = [0, 1]):
+    precisao_treinamento = []
+    # precisao_teste = []
+
+    for i in range(30):
+        pesos = inicializar_pesos(dominio_pesos) # Alterando os pesos em cada inicialização
+        x_treinamento, y_treinamento, x_teste, y_teste, x_validacao, y_validacao = dividir_dataframe(previsores, classe_nova, 0.7, 0.15, 0.15)
+
+        treinamento = treinar(epocas, funcao_ativacao, funcao_custo, pesos, x_treinamento, y_treinamento,
+                                     x_teste, y_teste)
+                                     
+        precisao_treinamento.append(max(treinamento))
+        # precisao_teste.append(max(treinamento[1]))
+
+
+    print('Melhor precisão de treinamento', max(precisao_treinamento))
+    print('Média de treinamento', np.mean(treinamento))
+    print('Desvio de treinamento', np.std(treinamento))
+
+    # print('Melhor precisão de teste', max(precisao_teste))
+
+executar_perceptron(funcao_ativacao_sigmoid, funcao_custo, 200, [-0.5, 0.5])
+
