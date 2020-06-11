5b5082acfb1060e73a79fe2c03e23275b034cc44
Alvarole <leandro0807@live.com>
2020-06-11 16:54:01 -0300

    cálculos funcionando

--- algoritmos-otimizacao/pso.py
+++ algoritmos-otimizacao/pso.py
@@ -21,53 +21,89 @@ def get_velocidade(n_particulas, dominio):
     
     return velocidade
 
-def atualizar_velocidade(v_atual, p_atual, melhor_p, aptidao, 
-                         dominio_v, ac1 = 2.05, ac2 = 2.05):
-    
-    v_aleatorio1 = [random.uniform(0, ac1) for i in range(len(p_atual[0]))] # baseado nas dimensões do problema
-    v_aleatorio2 = [random.uniform(0, ac2) for i in range(len(p_atual[0]))]
-    
+def atualizar_velocidade(v_atual, p_atual, melhor_p_particula, aptidao, 
+                         dominio_v, dominio_particulas, ac1 = 2.05, ac2 = 2.05):
+      
     v_nova = []
-
     count = 0
+    al1 = [1.6, 0.3, 1.3, 0.6] # [2, 1.6, 0.9, 1.6]
+    al2 = [1.8, 1.9, 0.2, 1.1] # [1, 0.3, 1.9, 2]
+
     for velocidade in v_atual:
-        melhor_p_vizinho = aptidao.index(min([aptidao[count - 1]] + [aptidao[ count + 1]]))
-        melhor_p_vizinho = p_atual[melhor_p_vizinho]
-
-        inteligencia_cognitiva = velocidade + np.dot(v_aleatorio1, (np.array(melhor_p) - np.array(p_atual[count])))
-
-        inteligencia_social = np.dot(v_aleatorio2, (np.array(melhor_p_vizinho) - np.array(p_atual[count])))
+        # v_aleatorio1 = [random.uniform(0, ac1) for i in range(len(dominio_particulas))] # baseado nas dimensões do problema
+        # v_aleatorio2 = [random.uniform(0, ac2) for i in range(len(dominio_particulas))]
         
-        v_nova.append(inteligencia_cognitiva + inteligencia_social)
+        v_aleatorio1 = al1[count]
+        v_aleatorio2 = al2[count]
+
+        proximo_vizinho = count + 1 if count + 1 < len(aptidao) else 0
+        melhor_p_vizinho = aptidao.index(min([aptidao[count - 1]] + [aptidao[proximo_vizinho]]))
+
+        melhor_p_vizinho = melhor_p_particula[melhor_p_vizinho]
+
+        inteligencia_cognitiva = velocidade + (v_aleatorio1 * (np.array(melhor_p_particula[count]) - np.array(p_atual[count]))) 
+
+        inteligencia_social = v_aleatorio2 * (np.array(melhor_p_vizinho) - np.array(p_atual[count]))
+        
+        velocidade_atualizada = inteligencia_cognitiva + inteligencia_social
+
+        velocidade_atualizada = velocidade_atualizada if velocidade_atualizada <= dominio_v[1] else dominio_v[1]
+        velocidade_atualizada = velocidade_atualizada if velocidade_atualizada >= dominio_v[0] else dominio_v[0]
+ 
+        v_nova.append(velocidade_atualizada)
         
         count += 1 
-
-    v_final = v_nova if v_nova <= dominio_v[1] else dominio_v[1]
-    v_final = v_nova if v_nova >= dominio_v[0] else dominio_v[0]
     
-    return v_final
+    return v_nova
 
-def atualizar_posicao(p_atual, nova_velocidade):
-    nova_p = p_atual + nova_velocidade
+def atualizar_posicao(p_atual, velocidade):
+    nova_p = np.array(p_atual) + np.array(velocidade)
     
     return nova_p
 
-def pso(n_particulas, dominio_particulas, dominio_velocidade):
-    enxame = get_enxame(n_particulas, dominio_particulas)
-    velocidade = get_velocidade(n_particulas, dominio_velocidade)
+def funcao_custo_teste(x):
+    return x**2
 
-    melhor_p = enxame
+
+def get_melhor_posicao(enxame_anterior, enxame, aptidao_anterior):
+    nova_aptidao = [funcao_custo_teste(x) for x in enxame]
+
+    count = 0
+    melhor_posicao = []
+    melhor_aptidao = []
+
+    for i in aptidao_anterior:
+        melhor_posicao.append(enxame[count] if nova_aptidao[count] < aptidao_anterior[count] else enxame_anterior[count])
+        melhor_aptidao.append(nova_aptidao[count] if nova_aptidao[count] > aptidao_anterior[count] else aptidao_anterior[count])
+        count +=1
+
+    return melhor_posicao, nova_aptidao, melhor_aptidao
+
+def pso(n_particulas, dominio_particulas, dominio_velocidade):
+    # enxame = get_enxame(n_particulas, dominio_particulas)
+    # velocidade = get_velocidade(n_particulas, dominio_velocidade)
+    enxame = [2, 3, -4, -2]
+    velocidade = [1, -2, 2, -1]
+
+    melhor_p_particula = enxame
+    enxame_anterior = []
     execucao = 0
 
-    while execucao < 30:
+    while execucao < 3:
         
-        aptidao = [funcao_aptidao(x, y) for x, y in enxame]
-        
-        melhor_p_global = enxame[aptidao.index(min(aptidao))]
-
-        velocidade = atualizar_velocidade(velocidade, enxame, melhor_p_global, aptidao, dominio_velocidade) 
+        if len(enxame_anterior) == 0:
+            aptidao = [funcao_custo_teste(x) for x in enxame]
+            velocidade = atualizar_velocidade(velocidade, enxame, melhor_p_particula,
+                                        aptidao, dominio_velocidade, dominio_particulas)
+        else:
+            melhor_p_particula, aptidao, melhor_aptidao = get_melhor_posicao(enxame_anterior, enxame, aptidao)            
+            velocidade = atualizar_velocidade(velocidade, enxame, melhor_p_particula,
+                                        melhor_aptidao, dominio_velocidade, dominio_particulas)
+         
+        enxame_anterior = enxame.copy()
+        enxame = atualizar_posicao(enxame, velocidade)
 
         execucao += 1
     return enxame
 
-pso(5, [(-5 ,5), (-5 ,5)], [-2, 2])
+pso(5, [(-7 ,7)], [-2, 2])
