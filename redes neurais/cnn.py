b63bc3868e7c4173079945a873a444c4a8754d0b
Alvarole <leandro0807@live.com>
2020-06-18 10:55:05 -0300

    melhorias no kernel

--- redes neurais/cnn.py
+++ redes neurais/cnn.py
@@ -14,15 +14,11 @@ image = image[:,:,0]
 kernel = np.asmatrix([[0, -1, 0], [-1,5,-1], [0,-1,0]]) # sharpen
 # kernel = np.asmatrix([[-1, -1, -1], [-1,8,-1], [-1,-1,-1]]) # top sobel
 
-x = np.expand_dims(kernel, axis=0)
-
 position = 3
 stride = 1
 padding = 1
 
 new_image = np.zeros(shape=(399, 300))
-
-# image = image[0:1332, 0:999]
 
 linha_inicial = 0
 linha_final = 3
@@ -31,11 +27,16 @@ while position < image.shape[0]:
     coluna_inicial = 0
     coluna_final = 3
     
-    for i in range(image.shape[1] - 3):
+    for i in range(image.shape[1]):
         kernel_area = image[linha_inicial:linha_final, coluna_inicial:coluna_final]
-        kernel_result = np.dot(x, kernel_area)
         
-        new_image[linha_inicial, coluna_inicial] = np.sum(kernel_result[0,:,:])
+        if kernel_area.shape != kernel.shape:
+            kernel_area = np.vstack([kernel_area, np.asmatrix([[0,0,0]])]) if kernel_area.shape[0] != kernel.shape[0] else kernel_area  
+            kernel_area = np.hstack([kernel_area, np.asmatrix([[0],[0],[0]])]) if kernel_area.shape[1] != kernel.shape[1] else kernel_area  
+
+        kernel_result = np.dot(kernel, kernel_area)
+        
+        new_image[linha_inicial, coluna_inicial] = np.sum(kernel_result)
     
         coluna_inicial += padding
         coluna_final += padding
