1e219efae6a74dc3e2cd5fb3fccaad5b9fb30362
Alvarole <leandro0807@live.com>
2020-06-22 22:36:41 -0300

    criação do dicionário de cidades e colonia de formigas

--- algoritmos-otimizacao/aco.py
+++ algoritmos-otimizacao/aco.py
@@ -11,6 +11,25 @@ def get_distancia_entre_pontos(cidade1, cidade2):
     distancia = math.sqrt(diferenca_coord)
     return distancia
 
+def get_dicionario_cidades(combinacao_cidades):
+    cidades = {}
+    for i in combinacao_cidades:
+        cidades[i] = [0, 0]
+
+    return cidades
+
+def iniciar_colonia(n_formigas, n_cidades):
+    colonia = []
+
+    for i in range(n_formigas):
+        colonia.append(random.randint(0, n_cidades))
+
+    return colonia
+
 combinacao_cidades = list(itertools.combinations(dataframe['index'].values, 2))
+
+cidades = get_dicionario_cidades(combinacao_cidades)
+
+formigas = iniciar_colonia(20, len(dataframe))
 
 print(get_distancia_entre_pontos([10,20], [30,40]))
