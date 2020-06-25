97aa4c944fd77c3a6a474d73a2ec13d10efe4cfe
Alvarole <leandro0807@live.com>
2020-06-24 22:27:34 -0300

    pegando probabilidade de movimento a partir da distância das cidades

--- algoritmos-otimizacao/aco.py
+++ algoritmos-otimizacao/aco.py
@@ -6,15 +6,15 @@ import itertools
 
 dataframe = pd.read_csv('dataset/berlin.csv')
 
-def get_distancia_entre_pontos(cidade1, cidade2):
-    diferenca_coord = abs(cidade1[0] - cidade2[0])**2 + abs(cidade1[1] - cidade2[1])**2
+def get_distancia_entre_pontos(cidade1, cidade2): # distância euclidiana 
+    diferenca_coord = (cidade1[0] - cidade2[0])**2 + (cidade1[1] - cidade2[1])**2
     distancia = math.sqrt(diferenca_coord)
     return distancia
 
 def get_dicionario_cidades(combinacao_cidades):
     cidades = {}
     for i in combinacao_cidades:
-        cidades[i] = [0, 0]
+        cidades[i] = [1] 
 
     return cidades
 
@@ -26,32 +26,45 @@ def iniciar_colonia(n_formigas, n_cidades):
 
     return colonia
 
-def get_cidades_vizinhas(formigas, cidades, dataframe, combinacao_cidades):
+def get_distancia_cidades_vizinhas(formigas, cidades, dataframe, combinacao_cidades):
     distancias = []
 
     for i in formigas:
         coordenadas_cidade = dataframe[dataframe['index'] == i]
         coordenadas_cidade = coordenadas_cidade.iloc[:, 1:3].values
         
-        cidades = dataframe[dataframe['index'] != i]
-        cidades = cidades.iloc[:, 1:3].values 
+        df_cidades = dataframe[dataframe['index'] != i]
+        df_cidades = df_cidades.values 
 
-        distancia_formiga = []      
+        distancia_formiga = {}      
         
-        for vizinho in cidades:
-            distancia_formiga.append(get_distancia_entre_pontos([coordenadas_cidade[0][0], coordenadas_cidade[0][1]], \
-                            [vizinho[0], vizinho[1]]))
+        for vizinho in df_cidades:                
+            distancia_formiga[(i, vizinho[0])] =  get_distancia_entre_pontos([coordenadas_cidade[0][0], coordenadas_cidade[0][1]], \
+                            [vizinho[1], vizinho[2]])
 
         distancias.append(distancia_formiga)
 
     return distancias
 
-combinacao_cidades = list(itertools.combinations(dataframe['index'].values, 2))
+def get_probabilidade_movimento(distancia_cidades_vizinhas, cidades, alfa = 1, beta = 5):
+    
+    for distancia in distancia_cidades_vizinhas:
+        p = []
+        for cidade in distancia:
+            inverso_distancia = 1 / distancia[cidade]
+            
+            p.append((cidades[cidade][0]**alfa) * (inverso_distancia**beta) / 1)
+
+    return p
+
+combinacao_cidades = list(itertools.permutations(dataframe['index'].values, 2))
 
 cidades = get_dicionario_cidades(combinacao_cidades)
 
 formigas = iniciar_colonia(20, len(dataframe))
 
-cidades_vizinhas = get_cidades_vizinhas(formigas, cidades, dataframe, combinacao_cidades)
+distancia_cidades_vizinhas = get_distancia_cidades_vizinhas(formigas, cidades, dataframe, combinacao_cidades)
+
+definir_movimento_formigas = get_probabilidade_movimento(distancia_cidades_vizinhas, cidades) # TODO: estou pegando a distância, mas falta os feromônios
 
 print(get_distancia_entre_pontos([10,20], [30,40]))
